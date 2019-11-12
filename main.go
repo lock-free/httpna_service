420f60f2048f2100fdffa8316d0038cd791c7a7b
loveKino <842914439@qq.com>
2019-11-12 23:22:06 +0800

    fix closure bug

--- main.go
+++ main.go
@@ -37,64 +37,66 @@ func main() {
 	// (2) get user info from callback by oauth
 	//     (getUserInfo, host, url, CallbackEndPoint)
 	for _, oauthConf := range httpNAConf.OAuth {
-		http.HandleFunc(oauthConf.LoginEndPoint, func(w http.ResponseWriter, r *http.Request) {
-			// get callback host
-			host := GetRedirectHost(r)
-			v, err := naPools.CallProxy(oauthConf.ServiceType,
-				pcpClient.Call("constructOAuthUrl", host, oauthConf.CallbackEndPoint),
-				2*time.Minute)
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				return
-			}
+		func(oauthConf httpna.OAuthConf) {
+			http.HandleFunc(oauthConf.LoginEndPoint, func(w http.ResponseWriter, r *http.Request) {
+				// get callback host
+				host := GetRedirectHost(r)
+				v, err := naPools.CallProxy(oauthConf.ServiceType,
+					pcpClient.Call("constructOAuthUrl", host, oauthConf.CallbackEndPoint),
+					2*time.Minute)
+				if err != nil {
+					w.Write([]byte(err.Error()))
+					return
+				}
 
-			url, ok := v.(string)
-			if !ok {
-				w.Write([]byte("unexpected worker error: url is not string"))
-				return
-			}
-			log.Printf("login redict url is: %s", url)
-			http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-		})
-
-		http.HandleFunc(oauthConf.CallbackEndPoint, func(w http.ResponseWriter, r *http.Request) {
-			host := GetRedirectHost(r)
-			user, err := naPools.CallProxy(oauthConf.ServiceType,
-				pcpClient.Call("getUserInfo", host, r.URL.String(), oauthConf.CallbackEndPoint),
-				2*time.Minute)
-
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				return
-			}
-
-			sessionUser := SessionUser{"google", user}
-			value, err := json.Marshal(sessionUser)
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				return
-			}
-
-			cypher, err := naPools.CallProxy("session_obrero", pcpClient.Call("encryptSession", string(value)), 120*time.Second)
-			if err != nil {
-				w.Write([]byte(err.Error()))
-				return
-			}
-			cypherText, ok := cypher.(string)
-			if !ok {
-				w.Write([]byte("unexpect type error for encryptSession"))
-				return
-			}
-
-			http.SetCookie(w, &http.Cookie{
-				Name:    httpNAConf.SESSION_COOKIE_KEY,
-				Value:   cypherText,
-				Path:    httpNAConf.SESSION_PATH,
-				Expires: time.Now().Add(time.Duration(httpNAConf.SESSION_EXPIRE) * time.Second),
+				url, ok := v.(string)
+				if !ok {
+					w.Write([]byte("unexpected worker error: url is not string"))
+					return
+				}
+				log.Printf("login redict url is: %s", url)
+				http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 			})
 
-			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		})
+			http.HandleFunc(oauthConf.CallbackEndPoint, func(w http.ResponseWriter, r *http.Request) {
+				host := GetRedirectHost(r)
+				user, err := naPools.CallProxy(oauthConf.ServiceType,
+					pcpClient.Call("getUserInfo", host, r.URL.String(), oauthConf.CallbackEndPoint),
+					2*time.Minute)
+
+				if err != nil {
+					w.Write([]byte(err.Error()))
+					return
+				}
+
+				sessionUser := SessionUser{"google", user}
+				value, err := json.Marshal(sessionUser)
+				if err != nil {
+					w.Write([]byte(err.Error()))
+					return
+				}
+
+				cypher, err := naPools.CallProxy("session_obrero", pcpClient.Call("encryptSession", string(value)), 120*time.Second)
+				if err != nil {
+					w.Write([]byte(err.Error()))
+					return
+				}
+				cypherText, ok := cypher.(string)
+				if !ok {
+					w.Write([]byte("unexpect type error for encryptSession"))
+					return
+				}
+
+				http.SetCookie(w, &http.Cookie{
+					Name:    httpNAConf.SESSION_COOKIE_KEY,
+					Value:   cypherText,
+					Path:    httpNAConf.SESSION_PATH,
+					Expires: time.Now().Add(time.Duration(httpNAConf.SESSION_EXPIRE) * time.Second),
+				})
+
+				http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			})
+		}(oauthConf)
 	}
 
 	// TODO dynamic proxy middlewares
